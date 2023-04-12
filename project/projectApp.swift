367690166f4e17983b160ee330e0df5f4b7f349a
Wong Sau Hang Alex <40209618+alxwong1991@users.noreply.github.com>
2023-04-12 16:26:07 +0800

    Initial Commit

---
+++ project/projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  projectApp.swift
+//  project
+//
+//  Created by Alex MAC on 12/4/2023.
+//
+
+import SwiftUI
+
+@main
+struct projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
